69a0788f35f92de00073e8c00aa36257b2dfa89c
Horvath Bator <horvathbator5@gmail.com>
2024-06-06 19:06:43 +0200

    kell a

--- HypeToBuy/settings.py
+++ HypeToBuy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-khz4j(%5ua!o1&utf+q0kx-38$bro#sq%dppy(9#p3w*^w)b!7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://hypetobuy-horvath-bators-projects.vercel.app/', '127.0.0.1', 'localhost']
 
